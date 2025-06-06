214f05221412277cb245076cc1de4989f6673fd2
Jan Willem Goedvolk <jan.goedvolk@student.saedubai.com>
2025-06-06 18:35:14 +0400

    Anvil Start
    *UNTESTED CODE*
    Started adding an anvil that follows the closest player to it, and falls on them if they stand under it for long enough.
    Added a script to find and follow the closest player
    Added a script to sense when players are near enough without using colliders or triggers (I don't know why, it would make things so much easier.). This uses events for when the sensor sucessfully senses a player, when the target changes, and conditions for whether it needs to sense for a certain amount of time first

---
+++ Assets/Scripts/Environment/Sensor.cs
@@ -0,0 +1,72 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace JW.Roguelike.Objects
+{
+    public class Sensor : MonoBehaviour
+    {
+        [Header("Target")]
+        [SerializeField] private Transform target;
+        [SerializeField] private float sensorDistance;
+
+        [Header("Conditions")]
+        [SerializeField] private bool isSensing = false;
+        [SerializeField] private bool requiresMinTimeSensed = false;
+        [Header("Time Based")]
+        [SerializeField] private float currentTimeSensed = 0f;
+        [SerializeField] private float minTimeSensed = 0.5f;
+        [SerializeField] private float sensedCooldown = -1f;
+        
+        [Header("Events")]
+        [SerializeField] private UnityEvent onSensed;
+        [SerializeField] private UnityEvent onTargetChanged;
+        
+        private void Update()
+        {
+            if (target == null)
+            {
+                return;
+            }
+
+            if (Vector2.Distance(transform.position, target.position) <= sensorDistance)
+            {
+                onSensed?.Invoke();
+                isSensing = true;
+            }
+            else
+            {
+                isSensing = false;
+            }
+
+            if (isSensing)
+            {
+                currentTimeSensed += Time.deltaTime;
+                if (currentTimeSensed >= minTimeSensed)
+                {
+                    
+                }
+            }
+        }
+
+        /// <summary>
+        /// Sets the target transform to sense for
+        /// </summary>
+        /// <param name="newTarget"></param>
+        public void SetTarget(Transform newTarget)
+        {
+            this.target = newTarget;
+            onTargetChanged?.Invoke();
+        }
+
+        private void OnDrawGizmos()
+        {
+            // Sensor range. Red when not in range, green when in range
+            if (target != null)
+            {
+                Gizmos.color = Vector2.Distance(transform.position, target.position) <= sensorDistance ? Color.red : Color.green;
+            }
+            Gizmos.DrawWireSphere(transform.position, sensorDistance);
+        }
+    }
+}
