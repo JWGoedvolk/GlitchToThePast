89b97a3ab67e6a5222fae35dd00df8dc46c320a7
Dana <tetekim31@gmail.com>
2025-06-19 20:36:18 +0400

    Attempted Input detection system.  character selection canvas set up
    First time using the new input system and I am so confused, I'll free up time tommorow to fix everything, the code isn't in a functional state right now, I'm kind of still seeing things through.. And looking for ways to wire functions into player input component. Also, faced an issue where multiple controllers were detected even though I just connected one? I tried filtering devices out, did not work as expected. I will also add comments and summaries tommorow.

---
+++ Assets/GlitchInThePast/Scripts/MainMenu/CharacterSelectionSystem/CharacterSelectionPanel.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CharacterSelectionPanel : MonoBehaviour
+{
+    [SerializeField] private InputConnectionManager inputConnectionManager;
+
+    void OnEnable()
+    {
+        // inputConnectionManager.ResetConfirmations();
+        inputConnectionManager.AssignInputs();
+    }
+
+    public void OnPlayer1Confirmed()
+    {
+        // inputConnectionManager.Player1Confirmed = true;
+        Debug.Log("Player 1 locked in!");
+    }
+
+    public void OnPlayer2Confirmed()
+    {
+        // inputConnectionManager.Player2Confirmed = true;
+        Debug.Log("Player 2 locked in!");
+    }
+}
---
+++ Assets/GlitchInThePast/Scripts/MainMenu/InputSystem/inputConnectionManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputConnectionManager : MonoBehaviour
+{
+    public enum InputType { None, Keyboard, Controller1, Controller2 }
+    [HideInInspector] public static InputType Player1InputType, Player2InputType;
+
+    public PlayerInput player1Input, player2Input;
+    public GameObject characterSelectionPanel;
+
+    bool hasAssigned = false;
+
+    void OnEnable()
+    {
+        InputSystem.onDeviceChange += OnDeviceChange;
+        hasAssigned = false;
+        TryAssignOnPanelOpen();
+    }
+
+    void OnDisable()
+    {
+        InputSystem.onDeviceChange -= OnDeviceChange;
+    }
+
+    void OnDeviceChange(InputDevice device, InputDeviceChange change)
+    {
+        if (!hasAssigned && characterSelectionPanel.activeInHierarchy
+            && device is Gamepad
+            && (change == InputDeviceChange.Added || change == InputDeviceChange.Removed))
+        {
+            AssignInputs();
+        }
+    }
+
+    void TryAssignOnPanelOpen()
+    {
+        if (characterSelectionPanel.activeInHierarchy && !hasAssigned)
+            AssignInputs();
+    }
+
+    public void AssignInputs()
+    {
+        var controllers = Gamepad.all.Where(g => g.added)  .GroupBy(g => g.description.product) .Select(grp => grp.First()) .ToList();
+
+        Debug.Log($"Connected Devices: {string.Join(", ", controllers.Select(g => g.description.product))}");
+
+        if (controllers.Count == 0)
+        {
+            Player1InputType = Player2InputType = InputType.None;
+            Debug.LogError("You need at least one controller!"); //TODO: Reflect this through UI.
+            return;
+        }
+
+        if (controllers.Count == 1)
+        {
+            Player1InputType = InputType.Keyboard;
+            Player2InputType = InputType.Controller1;
+
+            player1Input.SwitchCurrentControlScheme("Keyboard&Mouse", Keyboard.current, Mouse.current);
+            player2Input.SwitchCurrentControlScheme("Gamepad", controllers[0]);
+        }
+        else if(controllers.Count == 2)
+        {
+            Player1InputType = InputType.Controller2;
+            Player2InputType = InputType.Controller1;
+
+            player1Input.SwitchCurrentControlScheme("Gamepad", controllers[1]);
+            player2Input.SwitchCurrentControlScheme("Gamepad", controllers[0]);
+        }
+
+        hasAssigned = true;
+        Debug.Log($"[InputMgr] P1: {Player1InputType}, P2: {Player2InputType}");
+    }
+}
