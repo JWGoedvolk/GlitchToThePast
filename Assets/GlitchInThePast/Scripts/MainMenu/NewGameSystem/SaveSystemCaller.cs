fb7b6a4a4b7091137d1af4ba97c3e8d00d35f25a
Dana <tetekim31@gmail.com>
2025-06-20 06:58:53 +0400

    Temp new game -> opens character selection

--- Assets/GlitchInThePast/Scripts/MainMenu/NewGameSystem/SaveSystemCaller.cs
+++ Assets/GlitchInThePast/Scripts/MainMenu/NewGameSystem/SaveSystemCaller.cs
@@ -9,12 +9,15 @@ namespace GameData
 
     public class SaveSystemCaller : MonoBehaviour
     {
+        [SerializeField] private GameObject CharacterSelection;
+
         #region Public Functions
         #region New Game Button
         public void StartNewGame()
         {
             GameSaveData newData = new GameSaveData();
             GameSaveSystem.SaveGame(newData);
+            CharacterSelection.SetActive(true); // TODO: Let there be a way to exit the selection screen which also results in deleting the new game file.
             // TODO: Load the first scene (Narrartive here)
         }
         #endregion
