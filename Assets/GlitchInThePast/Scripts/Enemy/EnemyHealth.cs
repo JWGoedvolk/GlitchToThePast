da1010c4775a982ceb2e892ba06ca22ca7f90c02
Jan Willem Goedvolk <jan.goedvolk@student.saedubai.com>
2025-06-27 11:27:19 +0400

    Added Enemy Health System And Improved Enemy Spawner
    > Made test materials for melee and ranged enemies and removed the canvas for what enemy type they are so the designers can simply swap over the sprite and it should continue working.
    > Added a basic health system for the enemies that also track what enemy type they are
    > Made the spawner keep track of how many of each enemy is still alive
    > Added a variable to control the max number of each enemy type indepentintly

---
+++ Assets/GlitchInThePast/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,48 @@
+using TMPro;
+using UnityEngine;
+
+namespace Systems.Enemies
+{
+    public class EnemyHealth : MonoBehaviour
+    {
+        public enum EnemyTypes
+        {
+            Melee,
+            Ranged,
+            Boss
+        }
+        
+        [Header("Stats")] 
+        [SerializeField] private int healthMax = 3;
+        [SerializeField] private int healthCurrent = 3;
+        public EnemyTypes EnemyType = EnemyTypes.Melee;
+        public int Health
+        {
+            get { return healthCurrent; }
+            set
+            {
+                healthCurrent = value; 
+                
+                // Check if we died
+                if (healthCurrent <= 0)
+                {
+                    if (EnemyType == EnemyTypes.Melee)
+                    {
+                        spawner.MeleeEnemyCount--;
+                    }
+                    else if (EnemyType == EnemyTypes.Ranged)
+                    {
+                        spawner.RangedEnemyCount--;
+                    }
+                }
+                
+                // Update health UI
+                healthUI.localScale = new Vector3(healthCurrent/healthMax, 1, 1);
+            }
+        }
+        public EnemySpawner spawner; // We hold a reference to our spawner so we can keep track of how many of each enemy is currently alive
+        
+        [Header("UI")]
+        [SerializeField] private Transform healthUI;
+    }
+}
--- Assets/GlitchInThePast/Scripts/Enemy/EnemySpawner.cs
+++ Assets/GlitchInThePast/Scripts/Enemy/EnemySpawner.cs
@@ -11,12 +11,17 @@ namespace Systems.Enemies
         [SerializeField] private Transform meleeSpawnPoint;
         [SerializeField] private float meleeSpawnInterval = 1f;
         private float meleeSpawnTimer = 0f;
+        public int MeleeEnemyCount = 0;
+        [SerializeField] private int maxMeleeCount = 5;
     
         [Header("Ranged")]
         [SerializeField] private GameObject rangedEnemy;
         [SerializeField] private Transform rangedSpawnPoint;
         [SerializeField] private float rangedSpawnInterval = 1f;
         private float rangedSpawnTimer = 0f;
+        public int RangedEnemyCount = 0;
+        [SerializeField] private int maxRangedCount = 5;
+
 
         public void OnReset()
         {
@@ -27,19 +32,35 @@ namespace Systems.Enemies
         {
             if (!isActive) return; // Will only continue spawning when the lever is not pulled yet
             
-            // Increaase timers
+            // Increase timers
             meleeSpawnTimer += Time.deltaTime;
             rangedSpawnTimer += Time.deltaTime;
 
             // Check timers for spawn intervals
             if (meleeSpawnTimer >= meleeSpawnInterval)
             {
-                Instantiate(meleeEnemy, meleeSpawnPoint); // Spawn melee enemy at the melee spawn point as child of the spawn point
+                if (MeleeEnemyCount < maxMeleeCount)
+                {
+                    GameObject newMelee = Instantiate(meleeEnemy, meleeSpawnPoint); // Spawn melee enemy at the melee spawn point as child of the spawn point
+                    EnemyHealth enemyHealth = newMelee.GetComponent<EnemyHealth>(); // Get the health system to set it up
+                    enemyHealth.spawner = this;
+                    enemyHealth.EnemyType = EnemyHealth.EnemyTypes.Melee;
+                
+                    MeleeEnemyCount++;
+                }
                 meleeSpawnTimer = 0f;
             }
             if (rangedSpawnTimer >= rangedSpawnInterval)
             {
-                Instantiate(rangedEnemy, rangedSpawnPoint); // Spawn melee enemy at the melee spawn point as child of the spawn point
+                if (RangedEnemyCount < maxRangedCount)
+                {
+                    GameObject newRanged = Instantiate(rangedEnemy, rangedSpawnPoint); // Spawn melee enemy at the melee spawn point as child of the spawn point
+                    EnemyHealth enemyHealth = newRanged.GetComponent<EnemyHealth>(); // Get the health system to set it up
+                    enemyHealth.spawner = this;
+                    enemyHealth.EnemyType = EnemyHealth.EnemyTypes.Ranged;
+                
+                    RangedEnemyCount++;
+                }
                 rangedSpawnTimer = 0f;
             }
         }
