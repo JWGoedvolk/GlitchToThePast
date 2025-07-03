2e335271916f9650e8880ae0c92b77155e9fb938
BYER <tetekim31@gmail.com>
2025-07-03 19:36:10 +0400

    Brought back the settingss panel that was overriden through merges
    Made the event system's first button change depending on the open panel so that players are able to navigate through without losing any control in the scene. It still needs polishing because it is a bit awkward as of now.

--- Assets/GlitchInThePast/Scripts/UI/MainMenuButtons.cs
+++ Assets/GlitchInThePast/Scripts/UI/MainMenuButtons.cs
@@ -1,5 +1,7 @@
-
+using UnityEngine.EventSystems;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuButtons : MonoBehaviour
 {
@@ -7,28 +9,122 @@ public class MainMenuButtons : MonoBehaviour
 
     [SerializeField] private GameObject settingsPanel;
     [SerializeField] private GameObject quitConfirmationPanel;
+    [SerializeField] private GameObject firstSettingsButton;
+    [SerializeField] private GameObject firstQuitPanelButton;
+    [SerializeField] private GameObject firstMainMenuButton;
 
+    [SerializeField] private Image substitlesToggleImage;
+    [SerializeField] private Sprite subtitlesOnImage;
+    [SerializeField] private Sprite SpritesubtitlesOffImage;
+
+    [SerializeField] private TMP_Text sizeLabel;
+
+    //button locker
+    [SerializeField] UIBlocker buttonLocker;
+
+    private bool subtitlesOn;
+    private string[] sizes = new string[3];
+    private int selectedSize = 0;
     #endregion
+
+    private void Start()
+    {
+        sizes[0] = "SMALL";
+        sizes[1] = "MEDIUM";
+        sizes[2] = "LARGE";
+
+        //auto assigning the buttons since they are seralized
+        if (buttonLocker == null)
+        {
+            buttonLocker = GetComponent<UIBlocker>();
+        }
+    }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape) && quitConfirmationPanel.activeSelf != true) settingsPanel.SetActive(!settingsPanel.activeSelf);
+        if (Input.GetKeyDown(KeyCode.Escape) && !quitConfirmationPanel.activeSelf || Input.GetButtonDown("Cancel") && !quitConfirmationPanel.activeSelf)
+        {
+            settingsPanel.SetActive(!settingsPanel.activeSelf);
+
+            EventSystem.current.SetSelectedGameObject(null);
+            if (settingsPanel.activeSelf)
+                EventSystem.current.SetSelectedGameObject(firstSettingsButton);
+            else
+                EventSystem.current.SetSelectedGameObject(firstMainMenuButton);
+
+            UpdateButtonState();
+        }
+
     }
 
     // Options toggler with esc
     #region Settings
     public void SettingsToggler()
     {
-        settingsPanel.SetActive(!settingsPanel.activeSelf);
+        bool settingsActive = !settingsPanel.activeSelf;
+        settingsPanel.SetActive(settingsActive);
+
+        EventSystem.current.SetSelectedGameObject(null); 
+
+        if (settingsActive)
+        {
+            EventSystem.current.SetSelectedGameObject(firstSettingsButton);
+        }
+        else
+        {
+            EventSystem.current.SetSelectedGameObject(firstMainMenuButton);
+        }
+
+        // Calling the function.
+        UpdateButtonState();
+    }
+
+
+    public void GoToPreviousSize()
+    {
+        selectedSize = (selectedSize - 1 + sizes.Length) % sizes.Length;
+        UpdateSizeLabel();
+    }
+
+    public void GoToNextSize()
+    {
+        selectedSize = (selectedSize + 1) % sizes.Length;
+        UpdateSizeLabel();
+    }
+
+    private void UpdateSizeLabel()
+    {
+        if (sizeLabel is not null)
+        {
+            sizeLabel.text = sizes[selectedSize];
+        }
     }
     #endregion
 
+    #region Subtitles Toggler
+    // Just a visual changer
+    public void OnSubtitlesToggle()
+    {
+        subtitlesOn = !subtitlesOn;
+        if (substitlesToggleImage is not null)
+        {
+            substitlesToggleImage.sprite = subtitlesOn ? subtitlesOnImage : SpritesubtitlesOffImage;
+        }
+    }
+    #endregion
     // Quit button
     #region Quitting
     public void QuitGame()
     {
         quitConfirmationPanel.SetActive(true);
+
+        // Set focus to first button in quit confirmation panel
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(firstQuitPanelButton);
+
+        UpdateButtonState();
     }
+
 
     public void ConfirmQuit()
     {
@@ -38,6 +134,32 @@ public class MainMenuButtons : MonoBehaviour
     public void DismissQuit()
     {
         quitConfirmationPanel.SetActive(false);
+
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(firstMainMenuButton);
+
+        // calling the fucntion
+        UpdateButtonState();
+    }
+
+    #endregion
+
+    #region UIBlocking
+
+    private void UpdateButtonState()
+    {
+        //checks if any pannels are active
+        bool anyPannelActive = settingsPanel.activeSelf || quitConfirmationPanel.activeSelf;
+        if (anyPannelActive)
+        {
+            //if they are then call the fucntion from UIBlocker.cs
+            buttonLocker?.LockButton();
+        }
+        else
+        {
+            //if they are not then call........
+            buttonLocker?.UnlockButton();
+        }
     }
     #endregion
 }
