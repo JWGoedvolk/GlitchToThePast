3ccc4cc8aa3d50f9a2cb1153a80cdcfc48a29c95
Dana <tetekim31@gmail.com>
2025-08-08 00:09:58 +0400

    Disabled enemy prefabs on death so now enemies die yay!

--- Assets/GlitchInThePast/Scripts/Enemy/EnemyMovement/EnemyHealth.cs
+++ Assets/GlitchInThePast/Scripts/Enemy/EnemyMovement/EnemyHealth.cs
@@ -1,6 +1,5 @@
-using System;
+using System.Collections;
 using GlitchInThePast.Scripts.Player;
-using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -14,23 +13,32 @@ namespace Systems.Enemies
             Ranged,
             Boss
         }
-        
+
         // Stats
         [SerializeField] private int healthMax = 3;
         [SerializeField] private int healthCurrent = 3;
         public EnemyTypes EnemyType = EnemyTypes.Melee;
-        
+
+        // Flash parameters
+        [Tooltip("How long the sprite stays red when hit.")]
+        [SerializeField] private float flashDuration = 0.1f;
+        private SpriteRenderer spriteRenderer;
+        private Color originalColor;
+        private Coroutine flashCoroutine;
+
         // Events
         [SerializeField] public UnityEvent OnDamageTaken;
         [SerializeField] public UnityEvent OnDeath;
+
+        public EnemySpawner spawner;
+
         public int Health
         {
             get { return healthCurrent; }
             set
             {
-                healthCurrent = value; 
-                
-                // Check if we died
+                healthCurrent = value;
+
                 if (healthCurrent <= 0)
                 {
                     if (EnemyType == EnemyTypes.Melee)
@@ -47,29 +55,57 @@ namespace Systems.Enemies
             }
         }
         public int HealthMax => healthMax;
-        public EnemySpawner spawner; // We hold a reference to our spawner so we can keep track of how many of each enemy is currently alive
+
+        private void Awake()
+        {
+            spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+            if (spriteRenderer != null)
+                originalColor = spriteRenderer.color;
+        }
 
         public void TestMessage(int num)
         {
             TakeDamage(num);
         }
-        
+
         public void TakeDamage(int damage, PlayerWeaponSystem.WeaponType weaponType = PlayerWeaponSystem.WeaponType.None)
         {
             if (EnemyType == EnemyTypes.Boss)
             {
                 OnDamageTaken?.Invoke();
+                StartFlash();
             }
             else if (EnemyType == EnemyTypes.Melee && weaponType == PlayerWeaponSystem.WeaponType.Melee)
             {
                 Health -= damage;
                 OnDamageTaken?.Invoke();
+                StartFlash();
             }
             else if (EnemyType == EnemyTypes.Ranged && weaponType == PlayerWeaponSystem.WeaponType.Ranged)
             {
                 Health -= damage;
                 OnDamageTaken?.Invoke();
+                StartFlash();
             }
+        }
+
+        private void StartFlash()
+        {
+            if (spriteRenderer == null) return;
+            if (flashCoroutine != null)
+                StopCoroutine(flashCoroutine);
+            if (this.gameObject.activeSelf == true)
+            {
+                flashCoroutine = StartCoroutine(FlashRed());
+            }
+        }
+
+        private IEnumerator FlashRed()
+        {
+            spriteRenderer.color = Color.red;
+            yield return new WaitForSeconds(flashDuration);
+            spriteRenderer.color = originalColor;
+            flashCoroutine = null;
         }
     }
 }
