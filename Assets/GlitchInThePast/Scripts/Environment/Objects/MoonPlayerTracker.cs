f190bcddb2f3d4054f968c5fb0c52a9d19482c7b
Dana <tetekim31@gmail.com>
2025-08-07 10:29:31 +0400

    Imported some of my models into the game and set them up within the sections

---
+++ Assets/GlitchInThePast/Scripts/Environment/Objects/MoonPlayerTracker.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Environment.Moon
+{
+    public class MoonPlayerTracker : MonoBehaviour
+    {
+        // Looks at the closest player and tracks them. 
+        // If no players are in range then it'll go back to default pos.
+    }
+}
